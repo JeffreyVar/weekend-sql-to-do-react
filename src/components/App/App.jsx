46838416ccd220c5d3bbb6afb99545f180fb85f1
Jeffrey Varughese <jeffvarughese@gmail.com>
2023-08-13 17:37:38 -0500

    successfully added post route to router and app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 function App () {
 
-  // let [toDoListTask, setToDoListTask] = useState('');
+  let [toDoListTask, setToDoListTask] = useState('');
   let [toDoListArray, setToDoListArray] = useState([]);
 
   const fetchList = () => {
@@ -17,44 +17,40 @@ function App () {
       console.log(error);
     })
   }
+
+  const addTask = (event) => {
+    event.preventDefault();   
+    axios.post('/list', {task: toDoListTask})
+    .then((response) => {
+      console.log(response);
+      fetchList();
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
+
   useEffect(() => {
     fetchList();
-  }, [])
+  }, []);
 
-  // const addTask = (event) => {
-  //   event.preventDefault();
-  //   // console.log(`The person is ${famousPersonName} and they're famous for ${famousPersonRole}`);
+  return (
+    <div>
+      <h1>TO DO APP</h1>
     
-  //   // 
-
-  //   axios.post('/list', {task: toDoListTask})
-  //   .then((response) => {
-  //     console.log(response);
-  //     fetchList();
-  //   }).catch((error) => {
-  //     console.log(error);
-  //   });
-
-    return (
-      <div>
-        <h1>TO DO APP</h1>
-      
-        <h3>Add Task</h3>
-        {<form>
-          <label>Task</label>
-          {/* <input onChange={ (event) => setToDoListTask(event.target.value)}  /> */}
-          <button type="submit">Add Task</button>
-        </form>
-        }
-        <br></br>
-        <br></br>
-        <h3>My Tasks</h3>
+      <h3>Add Task</h3>
+      <form onSubmit={addTask}>
+        <label id="task">Task</label>
+        <input onChange={ (event) => setToDoListTask(event.target.value)}  />
+        <button>Add Task</button>
+      </form>
+      <br></br>
+      <br></br>
+      <h3>My Tasks</h3>
         <ul>
         {toDoListArray.map(task => (<li key={task.task} >{task.task}</li>))}
         </ul>
-      </div>
-    )
-  }
-
+    </div>
+  );
+};
 
 export default App
